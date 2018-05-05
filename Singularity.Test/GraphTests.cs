af98e535d74df7e7ea0e6b82dfdbd89e9af9a10e
Barsonax <rick-van-dam@outlook.com>
2018-05-05 14:18:08 +0200

    cleaned up graph update order tests

---
+++ Singularity.Test/GraphTests.cs
@@ -0,0 +1,107 @@
+﻿using Singularity.Graph;
+using Xunit;
+
+namespace Singularity.Test
+{
+	public class GraphTests
+	{
+		[Fact]
+		public void CheckUpdateOrder_Simple()
+		{
+			var nodeCollection = new TestNodeCollection();
+
+			var node1 = nodeCollection.Add("node1");
+			var node2 = nodeCollection.Add("node2");
+			var node3 = nodeCollection.Add("node3");
+
+			node3.Parents.Add(node2);
+			node2.Parents.Add(node1);
+
+			var graph = new Graph<TestNode>(nodeCollection);
+			var updateOrder = graph.GetUpdateOrder(node => node.Parents);
+
+			Assert.Equal(3, updateOrder.Length);
+
+			Assert.Equal(1, updateOrder[0].Length);
+			Assert.Equal(updateOrder[0][0], node1);
+
+			Assert.Equal(1, updateOrder[1].Length);
+			Assert.Equal(updateOrder[1][0], node2);
+
+			Assert.Equal(1, updateOrder[2].Length);
+			Assert.Equal(updateOrder[2][0], node3);
+		}
+
+		[Fact]
+		public void CheckUpdateOrder_Complex()
+		{
+			var nodeCollection = new TestNodeCollection();
+
+			var node1 = nodeCollection.Add("node1");
+			var node2 = nodeCollection.Add("node2");
+			var node3 = nodeCollection.Add("node3");
+			var node4 = nodeCollection.Add("node4");
+
+			node2.Parents.Add(node1);
+			node3.Parents.Add(node2);
+			node4.Parents.Add(node1);
+			node4.Parents.Add(node2);
+			node4.Parents.Add(node3);
+
+			var graph = new Graph<TestNode>(nodeCollection);
+			var updateOrder = graph.GetUpdateOrder(node => node.Parents);
+
+			Assert.Equal(4, updateOrder.Length);
+
+			Assert.Equal(1, updateOrder[0].Length);
+			Assert.Equal(updateOrder[0][0], node1);
+
+			Assert.Equal(1, updateOrder[1].Length);
+			Assert.Equal(updateOrder[1][0], node2);
+
+			Assert.Equal(1, updateOrder[2].Length);
+			Assert.Equal(updateOrder[2][0], node3);
+
+			Assert.Equal(1, updateOrder[3].Length);
+			Assert.Equal(updateOrder[3][0], node4);
+		}
+
+		[Fact]
+		public void CircularDependency_Simple_Throws()
+		{
+			var nodeCollection = new TestNodeCollection();
+
+			var node1 = nodeCollection.Add("node1");
+			var node2 = nodeCollection.Add("node2");
+
+			node1.Parents.Add(node2);
+			node2.Parents.Add(node1);
+
+			Assert.Throws<CircularDependencyException>(() =>
+			{
+				var graph = new Graph<TestNode>(nodeCollection);
+				var updateOrder = graph.GetUpdateOrder(node => node.Parents);
+			});
+		}
+
+		[Fact]
+		public void CircularDependency_Complex_Throws()
+		{
+			var nodeCollection = new TestNodeCollection();
+
+			var node1 = nodeCollection.Add("node1");
+			var node2 = nodeCollection.Add("node2");
+			var node3 = nodeCollection.Add("node3");
+
+			node1.Parents.Add(node3);
+			node2.Parents.Add(node1);
+			node3.Parents.Add(node2);
+
+			Assert.Throws<CircularDependencyException>(() =>
+			{
+				var graph = new Graph<TestNode>(nodeCollection);
+				var updateOrder = graph.GetUpdateOrder(node => node.Parents);
+			});
+		}
+	}
+}
---
+++ Singularity.Test/TestClasses/TestNode.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Singularity.Test
+{
+	public class TestNode
+	{
+		public string Name { get; }
+		public List<TestNode> Parents { get; }
+
+		public TestNode(string name)
+		{
+			Name = name;
+			Parents = new List<TestNode>();
+		}
+
+		public override string ToString()
+		{
+			return Name;
+		}
+	}
+
+	public class TestNodeCollection : IEnumerable<TestNode>
+	{
+		private List<TestNode> _nodes = new List<TestNode>();
+
+		public TestNode Add(string name)
+		{
+			var node = new TestNode(name);
+			_nodes.Add(node);
+			return node;
+		}
+
+		public IEnumerator<TestNode> GetEnumerator()
+		{
+			return _nodes.GetEnumerator();
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+	}
+}
