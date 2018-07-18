b5c6f08ed65ccc810194d093bf630b98bc74bbaf
Barsonax <rick-van-dam@outlook.com>
2018-07-18 15:40:14 +0200

    formatted singularityaggregrate exceptions to reflect the hierarchical nature of the error,  added some extra unit tests

---
+++ Singularity.Test/Exceptions/SingularityAggregrateExceptionTests.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace Singularity.Test.Exceptions
+{
+    public class SingularityAggregrateExceptionTests
+    {
+		[Fact]
+	    public void FirstLevel_IsCorrect()
+	    {
+			var exception = new SingularityAggregateException("1", new List<Exception>());
+
+		    var lines = exception.Message.Split(new[] {Environment.NewLine}, StringSplitOptions.None);
+			Assert.Equal("1",lines[0]);
+		}
+
+	    [Fact]
+	    public void SecondLevel_IsCorrect()
+	    {
+
+		    var exception = new SingularityAggregateException("1", new List<Exception>
+		    {
+			    new SingularityAggregateException("21", new List<Exception>
+			    {
+				    new Exception("211"),
+				    new Exception("212"),
+				    new Exception("213"),
+			    }),
+			    new SingularityAggregateException("22", new List<Exception>
+			    {
+				    new Exception("221"),
+				    new Exception("222"),
+				    new Exception("223"),
+			    })
+		    });
+
+		    var lines = exception.Message.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+		    Assert.Equal("1", lines[0]);
+
+		    Assert.Equal("	21", lines[1]);
+		    Assert.Equal("		211", lines[2]);
+		    Assert.Equal("		212", lines[3]);
+		    Assert.Equal("		213", lines[4]);
+
+		    Assert.Equal("	22", lines[5]);
+		    Assert.Equal("		221", lines[6]);
+		    Assert.Equal("		222", lines[7]);
+		    Assert.Equal("		223", lines[8]);
+	    }
+
+	    [Fact]
+	    public void ThirdLevel_IsCorrect()
+	    {
+
+		    var exception = new SingularityAggregateException("1", new List<Exception>
+		    {
+			    new SingularityAggregateException("21", new List<Exception>
+			    {
+				    new Exception("211"),
+				    new Exception("212"),
+				    new Exception("213"),
+			    }),
+			    new SingularityAggregateException("22", new List<Exception>
+			    {
+				    new Exception("221"),
+				    new Exception("222"),
+				    new Exception("223"),
+			    })
+		    });
+
+		    var lines = exception.Message.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+		    Assert.Equal("1", lines[0]);
+
+		    Assert.Equal("	21", lines[1]);
+		    Assert.Equal("		211", lines[2]);
+		    Assert.Equal("		212", lines[3]);
+		    Assert.Equal("		213", lines[4]);
+
+		    Assert.Equal("	22", lines[5]);
+		    Assert.Equal("		221", lines[6]);
+		    Assert.Equal("		222", lines[7]);
+		    Assert.Equal("		223", lines[8]);
+	    }
+
+	    [Fact]
+	    public void ThirdLevel_Mixed_IsCorrect()
+	    {
+
+		    var exception = new SingularityAggregateException("1", new List<Exception>
+		    {
+			    new SingularityAggregateException("21", new List<Exception>
+			    {
+				    new Exception("211"),
+				    new Exception("212"),
+				    new Exception("213"),
+			    }),
+			    new SingularityAggregateException("22", new List<Exception>
+			    {
+				    new Exception("221"),
+				    new Exception("222"),
+				    new Exception("223"),
+			    }),
+				new Exception("23"),
+			    new Exception("24"),
+			    new Exception("25")
+			});
+
+		    var lines = exception.Message.Split(new[] { Environment.NewLine }, StringSplitOptions.None);
+		    Assert.Equal("1", lines[0]);
+
+		    Assert.Equal("	21", lines[1]);
+		    Assert.Equal("		211", lines[2]);
+		    Assert.Equal("		212", lines[3]);
+		    Assert.Equal("		213", lines[4]);
+
+		    Assert.Equal("	22", lines[5]);
+		    Assert.Equal("		221", lines[6]);
+		    Assert.Equal("		222", lines[7]);
+		    Assert.Equal("		223", lines[8]);
+
+		    Assert.Equal("	23", lines[9]);
+		    Assert.Equal("	24", lines[10]);
+		    Assert.Equal("	25", lines[11]);
+		}
+	}
+}
--- Singularity/Exceptions/SingularityAggregateException.cs
+++ Singularity/Exceptions/SingularityAggregateException.cs
@@ -1,17 +1,49 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 
 namespace Singularity
 {
 	public class SingularityAggregateException : AggregateException
 	{
 		public string HeaderMessage { get; }
-		public override string Message => $"{HeaderMessage}:{Environment.NewLine}{string.Join(Environment.NewLine, InnerExceptions.Select(x => x.Message))}";
+		public override string Message => GenerateString(new StringBuilder(), 0, this).ToString();
+
+		private static StringBuilder GenerateString(StringBuilder builder, int indentLevel, AggregateException exception)
+		{
+			if (exception is SingularityAggregateException singularityAggregateException)
+			{
+				builder.Append(new string('	', indentLevel));
+				builder.AppendLine(singularityAggregateException.HeaderMessage);
+			}
+			indentLevel++;
+			foreach (var innerException in exception.InnerExceptions)
+			{
+
+
+				if (innerException is AggregateException aggregateException)
+				{
+					GenerateString(builder, indentLevel, aggregateException);
+				}
+				else
+				{					
+					foreach (var line in innerException.Message.Split(new[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries))
+					{
+						builder.Append(new string('	', indentLevel));
+						builder.AppendLine(line);
+					}					
+				}
+			}
+
+			return builder;
+		}
 
 		public SingularityAggregateException(string message, IEnumerable<Exception> innerExceptions) : base(innerExceptions)
 		{
 			HeaderMessage = message;
+
+
 		}
 	}
 }
