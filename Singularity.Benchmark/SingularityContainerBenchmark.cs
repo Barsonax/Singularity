1203a20b2003205bb37e1086860529a83f52332b
Rick <rick-van-dam@outlook.com>
2019-03-27 20:30:17 +0100

    Added a benchmark with di with no container to compare performance to direct calls

---
+++ Singularity.Benchmark/SingularityContainerBenchmark.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using BenchmarkDotNet.Attributes;
+using Singularity.Benchmark.TestClasses;
+using Singularity.TestClasses.Benchmark;
+
+namespace Singularity.Benchmark
+{
+    [Config(typeof(CustomJob))]
+    [MemoryDiagnoser]
+    public class SingularityContainerBenchmark
+    {
+        private readonly ContainerBenchmark _containerBenchmark = new ContainerBenchmark();
+
+        [GlobalSetup]
+        public void Setup()
+        {
+            _containerBenchmark.Setup();
+        }
+
+        [Benchmark]
+        public ISingleton1 Singleton()
+        {
+            return _containerBenchmark.Singleton();
+        }
+
+        [Benchmark]
+        public ITransient1 Transient()
+        {
+            return _containerBenchmark.Transient();
+        }
+
+        [Benchmark]
+        public ICombined1 Combined()
+        {
+            return _containerBenchmark.Combined();
+        }
+
+        [Benchmark]
+        public IComplex1 Complex()
+        {
+            return _containerBenchmark.Complex();
+        }
+
+        [Benchmark]
+        public IDisposable Disposable()
+        {
+            return _containerBenchmark.Disposable();
+        }
+
+        [Benchmark]
+        public void Register()
+        {
+            _containerBenchmark.Register();
+        }
+
+        [Benchmark]
+        public void RegisterAndEnumerate()
+        {
+            _containerBenchmark.RegisterAndEnumerate();
+        }
+
+        [Benchmark]
+        public Container NewContainer()
+        {
+            return _containerBenchmark.NewContainer();
+        }
+
+        [Benchmark]
+        public Container NewContainerFromCachedConfig()
+        {
+            return _containerBenchmark.NewContainerFromCachedConfig();
+        }
+
+        [Benchmark]
+        public IComplex1 NewContainerAndResolve()
+        {
+            return _containerBenchmark.NewContainerAndResolve();
+        }
+
+        [Benchmark]
+        public Container NewNestedContainer()
+        {
+            return _containerBenchmark.NewNestedContainer();
+        }
+    }
+}
