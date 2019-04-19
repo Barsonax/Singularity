60ce53f43fe860ef2b6422fa1275d63506189c2b
Rick <rick-van-dam@outlook.com>
2019-04-19 17:11:19 +0200

    cleanup

--- Singularity/Collections/SinglyLinkedListNode.cs
+++ Singularity/Collections/SinglyLinkedListNode.cs
@@ -1,6 +1,6 @@
 ﻿namespace Singularity.Collections
 {
-    public sealed class SinglyLinkedListNode<T>
+    internal sealed class SinglyLinkedListNode<T>
         where T : class
     {
         public static readonly SinglyLinkedListNode<T> Empty = new SinglyLinkedListNode<T>();
