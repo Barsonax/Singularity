c5364f5542d6f0fafa1db079f5edb1d731fe1275
Rick van Dam <rick-van-dam@outlook.com>
2020-03-11 23:39:47 +0100

    cleaning up memory tests

--- src/Tests/Singularity.Test/Injection/MemoryTests.cs
+++ src/Tests/Singularity.Test/Injection/MemoryTests.cs
@@ -1,19 +1,55 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.Loader;
+using System.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Singularity.Test.Utils;
+using Singularity.TestClasses.TestClasses;
 using Xunit;
 
 namespace Singularity.Test.Injection
 {
     public class MemoryTests
     {
+        [ReleaseOnlyFact]
+        public void ScopedInstanceIsReleased()
+        {
+            CheckIfCleanedUp(() =>
+            {
+                //ARRANGE
+                var container = new Container(c =>
+                {
+                    c.Register<Disposable>(s => s.With(ServiceAutoDispose.Always));
+                });
+
+                //ACT
+                var weakRef = new WeakReference(container.GetInstance<Disposable>());
+                container.Dispose();
+
+                //ASSERT
+                return weakRef;
+            });
+        }
+
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        private void CheckIfCleanedUp(Func<WeakReference> func)
+        {
+            var weakRef = func.Invoke();
+
+            for (int i = 0; weakRef.IsAlive && i < 10; i++)
+            {
+                GC.Collect(2, GCCollectionMode.Forced, true);
+                GC.WaitForPendingFinalizers();
+            }
+
+            Assert.False(weakRef.IsAlive);
+        }
+
         class UnloadableLoadContext : AssemblyLoadContext
         {
             public UnloadableLoadContext() : base(true) { }
@@ -31,27 +67,25 @@ namespace Singularity.Test.Injection
         [ReleaseOnlyFact]
         public void Foo()
         {
-            var references = new List<MetadataReference>() {
-                MetadataReference.CreateFromFile(typeof(Binder).Assembly.Location),
-                MetadataReference.CreateFromFile(AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.FullName.Substring(0, x.FullName.IndexOf(',')) == "netstandard").Location),
-                MetadataReference.CreateFromFile(AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.FullName.Substring(0, x.FullName.IndexOf(',')) == "System.Console").Location),
-                MetadataReference.CreateFromFile(AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.FullName.Substring(0, x.FullName.IndexOf(',')) == "System.Runtime").Location)
-            };
-
-            var csTree = CSharpSyntaxTree.ParseText("class A { public void B() { System.Console.WriteLine(\"hello\"); } }", parseOpts);
-            var asmName = Guid.NewGuid().ToString();
-            Compilation compilation = CSharpCompilation.Create(asmName, options: options, references: references).AddSyntaxTrees(csTree);
-
-            using (var ms = new MemoryStream())
+            CheckIfCleanedUp(() =>
             {
-                var emitResult = compilation.Emit(ms);
+                var references = new List<MetadataReference>()
+                {
+                    MetadataReference.CreateFromFile(typeof(Binder).Assembly.Location),
+                    MetadataReference.CreateFromFile(AppDomain.CurrentDomain.GetAssemblies().FirstOrDefault(x => x.FullName.Substring(0, x.FullName.IndexOf(',')) == "netstandard").Location),
+                };
+
+                var csTree = CSharpSyntaxTree.ParseText("class A { }", parseOpts);
+                Compilation compilation = CSharpCompilation.Create(Guid.NewGuid().ToString(), options: options, references: references).AddSyntaxTrees(csTree);
+
+                using var ms = new MemoryStream();
+                compilation.Emit(ms);
                 ms.Seek(0, SeekOrigin.Begin);
 
                 var alc = new UnloadableLoadContext();
-                var alcWeakRef = new WeakReference(alc);
+                var weakRef = new WeakReference(alc);
 
                 var asm = alc.LoadFromStream(ms);
-                var mthd = asm.GetType("A").GetMethod("B");
                 using (var container = new Container(c =>
                 {
                     c.ConfigureSettings(s =>
@@ -60,25 +94,12 @@ namespace Singularity.Test.Injection
                     });
                 }))
                 {
-                    using (var scope = container.BeginScope())
-                    {
-                        var inst = container.GetInstance(asm.GetType("A"));
-
-                        mthd.Invoke(inst, null);
-                    }
-
+                    var inst = container.GetInstance(asm.GetType("A"));
                 }
                 alc.Unload();
 
-
-                for (int i = 0; alcWeakRef.IsAlive && (i < 10); i++)
-                {
-                    GC.Collect(2, GCCollectionMode.Forced, true);
-                    GC.WaitForPendingFinalizers();
-                }
-
-                Assert.False(alcWeakRef.IsAlive);
-            }
+                return weakRef;
+            });
         }
     }
 }
